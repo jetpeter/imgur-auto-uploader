d15321fa0db7a70ec57432218dbf104b62581dc7
Jefferey Petersen <jffryptrsn@gmail.com>
2015-07-19 16:22:52 -0700

    add application class

---
+++ app/src/main/java/me/jefferey/screenshotuploader/ScreenshotUploaderApplication.java
@@ -0,0 +1,9 @@
+package me.jefferey.screenshotuploader;
+
+import android.app.Application;
+
+/**
+ * Created by jpetersen on 7/19/15.
+ */
+public class ScreenshotUploaderApplication extends Application {
+}
--- app/src/main/java/me/jefferey/screenshotuploader/ui/MainActivity.java
+++ app/src/main/java/me/jefferey/screenshotuploader/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package me.jefferey.screenshotuploader;
+package me.jefferey.screenshotuploader.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import me.jefferey.screenshotuploader.R;
 
 public class MainActivity extends AppCompatActivity {
 
